7cdd0d6dd1e77aaa086106a7e6d97688f884ea3b
joonajo <joona.joenpolvi@gmail.com>
2020-03-30 14:51:24 +0300

    Add some content to "home" page

--- pages/index/index.tsx
+++ pages/index/index.tsx
@@ -1,15 +1,37 @@
 import React from 'react'
 
 import { NextPage } from 'next'
+import styled from 'styled-components'
 
-const Index: NextPage = (): JSX.Element => {
+const AppWrapper = styled.div`
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+`
+
+const Title = styled.h1`
+    font-weight: normal;
+`
+
+const Text = styled.p`
+    
+`
+
+const App: NextPage = (): JSX.Element => {
     React.useEffect(() => {
         document.title = "NEXT-REACT-TS"
     }, [])
 
     return (
-        <h1>NEXT-REACT-TS-STYLED</h1>
+        <AppWrapper >
+            <Title>Hello!</Title>
+            <Text>NextJS x React x TypeScript x Styled-components</Text>
+        </AppWrapper>
     )
 }
 
-export default Index
+export default App
